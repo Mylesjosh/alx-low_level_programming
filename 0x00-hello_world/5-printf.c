ae3f24f792c61fa26916aef67ebf2f09ce3bf073
Mylesjosh <joshuaejiobih@gmail.com>
2022-06-02 15:57:21 -0700

    My first script

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-nclude <stdio.h>
+nclude < stdio.h >
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 } 
