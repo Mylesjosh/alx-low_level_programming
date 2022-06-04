f7020758442a5a845a473612ef39cd051acf1e1d
Mylesjosh <joshuaejiobih@gmail.com>
2022-06-04 10:39:52 -0700

    My first script

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,2 @@
-#include <stdio.h>
-
-/**
- * main - main block
- * Return:0
- */
-
-int main(void)
-{
-	int ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-	if (ch == e || ch == q)
-	{
-	grep -v [eq];
-	}
-	putchar (ch);
-	}
-	putchar('\n');
-	return (0);
-
-}
-
+#!/bin/bash
+4-print_alpha.c|grep [eq]
