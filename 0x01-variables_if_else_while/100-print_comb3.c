6f567d10c3a1e451d5c51d6cc5ae25c5e5aa8638
Mylesjosh <joshuaejiobih@gmail.com>
2022-06-05 13:45:49 -0700

    My first script

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (num1 = '0'; num1 <= '9'; num1++)
 	{
-	for (num2 = num1 + '1'; num2 <= '9'; num2++)
+	for (num2 = num1 + 1; num2 <= '9'; num2++)
 	{
 	putchar(num1);
 	putchar(num2);
@@ -26,6 +26,7 @@ int main(void)
 	}
 	}
 	return (0);
+	putchar('\n');
 
 }
 
