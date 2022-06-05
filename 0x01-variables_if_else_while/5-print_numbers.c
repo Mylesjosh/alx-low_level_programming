86e42ecb08ed31dfa66699b2e242b873243551ff
Mylesjosh <joshuaejiobih@gmail.com>
2022-06-04 21:55:54 -0700

    My first script

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return:0
+ */
+
+int main (void)
+{
+	int I;
+	for (I = 0; I < 10; I++)
+	{
+		printf(I);
+	}
+	printf('\n');
+	return (0);
+
+}
+
