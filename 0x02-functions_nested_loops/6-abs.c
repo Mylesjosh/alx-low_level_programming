0e3d57b84ad69399036cb94746177a9f422820c7
Mylesjosh <joshuaejiobih@gmail.com>
2022-06-08 14:36:05 -0700

    My first script

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @r: the integer to be computed
+ * Return: returns r(i.e. the absolute value of an integer.)
+ */
+
+int _abs(int r)
+{
+	if ( r >= 0)
+	{
+		return (r);
+	}
+
+	else
+	{
+		return (-r);
+	}
