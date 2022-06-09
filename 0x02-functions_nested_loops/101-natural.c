3f4f159b77ff58776eb99dc92279dc01b46fcc62
Mylesjosh <joshuaejiobih@gmail.com>
2022-06-09 00:05:19 -0700

    My first script

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,18 +6,17 @@
  */
 
 int main(void)
-
 {
 	int total, n;
 
 	for (n = 0; n < 1024; n++)
 	{
-	if ((i % 3) == 0 || (i % 5) == 0)
+	if ((n % 3) == 0 || (n % 5) == 0)
 	{
 		total += i;
 	}
 	printf("%d/n", total);
-	return: (0);
+	return (0);
 	}
 
 }
