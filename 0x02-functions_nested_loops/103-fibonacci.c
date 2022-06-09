836deb98f5bf3cf5da10803a9b2e348f7a68fa1b
Mylesjosh <joshuaejiobih@gmail.com>
2022-06-09 01:07:11 -0700

    My first script

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * return:
+ */
+
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, fibsum;
+	while (1);
+	{
+
+	float total_sum;
+	fibsum = fib1 + fib2;
+
+	if (fibsum > 4000000)
+	{
+		break;
+	}
+
+	if ((fibsum % 2) == 0)
+	{
+		total_sum += fibsum;
+	}
+
+	fib1 = fib2;
+	fib2 = fibsum;
+	}
+	printf("%.0f\n", tot_sum);
+	return (0);
+}
+
