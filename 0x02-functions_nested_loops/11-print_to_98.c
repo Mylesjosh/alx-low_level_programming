12712b7dc33ed26f27918c2a5b5ca40c40f152aa
Mylesjosh <joshuaejiobih@gmail.com>
2022-06-08 22:53:39 -0700

    My first script

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * prints all natural numbers from n to 98
+ * print_to_98 - prints all natural numbers from n to 98
  * @n: the number to begin counting at
  */
 
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,16 +7,17 @@
 void jack_bauer(void)
 {
 	int hour, min;
-	for (hour = 0; hour <= 23; hour++)
+	for (hour = 0; hour <= 23; hour+;)
 	{
-		_putchar((hour / 10) + '0');
-		_putchar((hour % 10) + '0');
 
 		for (min = 0; min <= 59; min++)
 		{
-			_putchar(':');
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
+
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
 			_putchar('\n');
 		}
 	}
