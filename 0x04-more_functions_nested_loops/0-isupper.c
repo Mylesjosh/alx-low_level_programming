b60c61cd5af9c655fe80498e8a477b7dc3b6dd04
Mylesjosh <joshuaejiobih@gmail.com>
2022-06-10 16:16:28 -0700

    nested

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - checks for uppercase alphabets
+ * @c: the integer to be checked
+ * Return: 1 or 0 depending on conditions
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{	
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
