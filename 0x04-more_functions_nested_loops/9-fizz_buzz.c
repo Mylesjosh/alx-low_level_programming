7172db2a80f3243fb8a0c9223e71f48773dbd98a
Mylesjosh <joshuaejiobih@gmail.com>
2022-06-10 20:47:51 -0700

    nested

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 	int num;
 
 	for (num = 1; num <= 100; num++)
-	{	
+	{
 		if ((num % 3) == 0 && (num % 5) == 0)
 		{
 		printf("FizzBuzz");
@@ -36,7 +36,7 @@ int main(void)
 	}
 	printf("\n");
 
-	Return (0);
+	return (0);
 
 }
 
