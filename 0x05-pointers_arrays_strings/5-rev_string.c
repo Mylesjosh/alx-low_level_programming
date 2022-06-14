694e0bcce44306905e3d5c0b358bac5c159ab182
Mylesjosh <joshuaejiobih@gmail.com>
2022-06-14 07:40:36 -0700

    pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -21,9 +21,9 @@ void rev_string(char *s)
 	length2 = length1 - 1;
 	for (i = 0; i < length1 / 2; i++)
 	{
-		tmp = s[i];
+		word = s[i];
 		s[i] = s[length2];
-		s[length] = wor;
+		s[length] = word;
 		length -= 1;
 	}
 
