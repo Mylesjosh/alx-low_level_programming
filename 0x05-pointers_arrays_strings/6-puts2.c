d2ec485b35a9d19d30bdb2a931ecf010b70551ff
Mylesjosh <joshuaejiobih@gmail.com>
2022-06-14 09:25:07 -0700

    pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,7 +3,7 @@
 
 /**
  * puts2 - function puts
- * @str - puts string
+ * @str: puts string
  */
 
 void puts2(char *str)
@@ -12,7 +12,7 @@ void puts2(char *str)
 
 	len = strlen(str);
 
-	for (i = 0; i < len; i++)
+	for (i = 0; i < len; i+=2)
 	{
 		_putchar(str[i]);
 	}
