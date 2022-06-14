096a0d88428ecf3859dd55135dc54d1665e64c36
Mylesjosh <joshuaejiobih@gmail.com>
2022-06-14 12:53:32 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_array - prints the elements of an array
+ * @a: pointer to array name
+ * @n: param n
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	_putchar('\n');
+}
