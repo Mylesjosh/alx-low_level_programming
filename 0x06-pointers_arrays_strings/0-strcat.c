d5c3b37ffa46310c6c9ec762777ecadaa7c32e6f
Mylesjosh <joshuaejiobih@gmail.com>
2022-06-15 12:25:20 -0700

    pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,11 +13,11 @@ char *strcat(char *dest, const char *src)
 
 	while (dest[index++])
 	{
-		dest_len++;
+		destlen++;
 	}
 	for (index = 0; src[index]; index++)
 	{
-		dest[dest_len++] = src[index];
+		dest[destlen++] = src[index];
 	}
 	return (dest);
 }
