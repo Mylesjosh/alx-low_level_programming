24bf27f070d0eac3e1c51c8cd0c3304e53d9ba80
Mylesjosh <joshuaejiobih@gmail.com>
2022-06-22 15:20:13 -0700

    pointers and grit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strspn - prints the prefix of a substring with respect to accept
+ * _strspn - prints the prefix of a substring with respect to accept
  * @s: main string
  * @accept: prefix evaluator string
  * Return: int
@@ -9,7 +9,7 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int a = 0, b, c = 0;
+	unsigned int a = 0, b, c = 0;
 
 	while (accept[a])
 		a++;
@@ -18,7 +18,8 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		if (accept[a] == s[b])
 		{
-			c+=1;
+			c += 1;
 		}
 	}
 	return (c);
+}
