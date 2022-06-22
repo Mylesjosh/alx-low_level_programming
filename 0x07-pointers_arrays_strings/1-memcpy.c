06f995362e0400c332ae73650db4771aa2b9e114
Mylesjosh <joshuaejiobih@gmail.com>
2022-06-22 12:51:58 -0700

    pointers and grit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy memory
+ * @dest: destination char
+ * @src: source char
+ * @n: int
+ * Return: a character
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int temp, i;
+
+	for (i = 0; i < n; i++)
+		temp[i] = dest;
+		dest[i] = src;
+
+	return (dest);
+}
+
