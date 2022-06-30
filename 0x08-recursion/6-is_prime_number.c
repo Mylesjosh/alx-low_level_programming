50e2464dd89e7dfeaa6fb8ee392d39f79361efc3
Mylesjosh <joshuaejiobih@gmail.com>
2022-06-29 17:13:57 -0700

    recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,14 +3,14 @@
 int check_prime(int, int);
 
 /**
- *check_prime - computes the square root of a number
+ * is_prime_number - computes the square root of a number
  * @n: the integer number
  * Return: prime
  */
 
 int is_prime_number(int n)
 {
-  return (check_prime(n, 1));
+ return (check_prime(n, 1));
 }
 
 /**
@@ -27,6 +27,6 @@ int check_prime(int n, int i)
 	if (n % 1 == 0 && i > 1)
 		return (0);
 	if ((n / i) < i)
-		return(1);
+		return (1);
 	return (check_prime(n, i + 1));
 }
