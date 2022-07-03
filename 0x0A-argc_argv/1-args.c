341891f4a3394be19338ec79d27f46ca09c7d16d
Mylesjosh <joshuaejiobih@gmail.com>
2022-07-03 08:41:45 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  main - main block
+ *  @argc: argument count
+ *  @argv: argument vector
+ *  Return:0
+ */
+
+int main(int argc, char argv[0])
+{
+	printf("%d\n", argc);
+
+	(void)argv[0];
+
+	return (0);
+}
