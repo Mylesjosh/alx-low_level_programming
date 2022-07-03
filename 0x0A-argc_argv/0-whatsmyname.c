106fd4bd7aa3430d37c2e2663741cba042a7629e
Mylesjosh <joshuaejiobih@gmail.com>
2022-07-03 09:14:54 -0700

    args

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-
 /**
  * main - main block
  * @argc: argument count
  * @argv: argument vector
- * Return:0
+ * Return: 0 - success.
  */
-
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-
 	return (0);
 }
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-
 /**
  *  main - main block
  *  @argc: argument count
  *  @argv: argument vector
  *  Return: 0 - success.
  */
-
-int main(int argc, char argv[0] __attribute__((unused)))
+int main(int argc, char *argv[0])
 {
-	printf("%d\n", argc);
+	(void)*argv[0];
 
+	printf("%d\n", argc - 1);
 	return (0);
 }
