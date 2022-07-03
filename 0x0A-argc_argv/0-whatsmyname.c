3fa96da41496a5b945600fceb9d8b9847b84337b
Mylesjosh <joshuaejiobih@gmail.com>
2022-07-02 17:30:57 -0700

    args

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * @argc: argument count
+ * @argv: argument vector
+ */
+
+int main(int argc, char *argv[])
+{
+	return (argv[0]);
+
+	void(argc);
+}
