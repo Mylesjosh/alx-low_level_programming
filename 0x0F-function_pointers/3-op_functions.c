b0d9fd942c97390bad59d2744e24f968fd56f714
Mylesjosh <joshuaejiobih@gmail.com>
2022-07-11 21:09:16 -0700

    functions

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -8,7 +8,7 @@
  */
 int op_add(int a, int b)
 {
-	return(a+b);
+	return (a + b);
 }
 
 /**
@@ -48,7 +48,7 @@ int op_div(int a, int b)
  * op_mod - calculates the remainder of the division of two integers
  * @a: first integer
  * @b: second integer
- * Return: remainder of a divided 
+ * Return: remainder of a divided
  */
 int op_mod(int a, int b)
 {
