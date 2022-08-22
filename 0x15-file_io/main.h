7d73227fbd473d6cad38dd438ed9efbb39abb455
Mylesjosh <joshuaejiobih@gmail.com>
2022-08-21 18:27:55 -0700

    file io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,8 +3,7 @@
 
 #include <sys/types.h>  
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h> 
+#include <fcntl.h> 
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
