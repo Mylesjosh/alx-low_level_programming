9ede01a1672f5ba3e738fa4c9c337232f18a073d
Mylesjosh <joshuaejiobih@gmail.com>
2022-08-21 18:23:19 -0700

    file io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t r, w, o;
 	*char buffer;
 
-	buffer = malloc(sizeof(char * letters);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
 	if (filename == NULL)
 		return (0);
 
